c3cae7ec6810b453f55a7ee909eb50a162cdb316
solennemz <smazabraud@asga.com>
2021-08-25 15:54:01 +0200

    ->addFlash pour un raccourics de qualité

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -16,7 +16,7 @@ class CartController extends AbstractController
     /**
      * @Route("/cart/add/{id}", name="cart_add", requirements={"id":"\d+"})
      */
-    public function add($id, ProductRepository $productRepository, SessionInterface $session, FlashBagInterface $flashBag)
+    public function add($id, ProductRepository $productRepository, SessionInterface $session)
     {
         // 0. Sécurisation : est-ce que le produit existe ? + requiremetns en annotation
         $product = $productRepository->find($id);
@@ -40,7 +40,7 @@ class CartController extends AbstractController
         // 6. Enregistrer le tableau mis à jour dans les sessions
         $session->set('cart', $cart);
 
-        $flashBag->add('success', "Le produit a bien été enregistré");
+        $this->addFlash('success', "Le produit a bien été enregistré");
 
         return $this->redirectToRoute('product_show', [
             'category_slug' => $product->getCategory()->getSlug(),

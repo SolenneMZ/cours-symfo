ffbf8b797ed932c6e22cc329cd3d2c0eb2c35c61
solennemz <smazabraud@asga.com>
2021-08-25 15:19:39 +0200

    méthode d'ajout de produit

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,19 +2,45 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
     /**
-     * @Route("/cart", name="cart")
+     * @Route("/cart/add/{id}", name="cart_add", requirements={"id":"\d+"})
      */
-    public function index(): Response
+    public function add($id, ProductRepository $productRepository, SessionInterface $session)
     {
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+        // 0. Sécurisation : est-ce que le produit existe ? + requiremetns en annotation
+        $product = $productRepository->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException("Le produit $id n'existe pas !");
+        }
+
+        // 1. Retrouver le panier dans la session (sous forme de tableau)
+        // 2. Si il n'existe pas encore, alors prendre un tableau vide
+        $cart = $session->get('cart', []);
+        // 3. Voir si le produit ($id) existe déjà dans le tableau
+        // 4. Si c'est le cas, simplement augmenter la quantité
+        // 5. Sinon, ajouter le produit avec la quantité
+        if (array_key_exists($id,$cart)) {
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+        }
+
+        // 6. Enregistrer le tableau mis à jour dans les sessions
+        $session->set('cart', $cart);
+
+        return $this->redirectToRoute('product_show', [
+            'category_slug' => $product->getCategory()->getSlug(),
+            'slug' => $product->getSlug()
         ]);
     }
 }

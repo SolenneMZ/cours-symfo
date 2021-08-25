38ea078d86be18ce35d1384d56d8ee4f30c3a989
solennemz <smazabraud@asga.com>
2021-08-25 11:58:05 +0200

    auth finally done

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -14,22 +12,25 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login", name="security_login")
      */
-    public function login(AuthenticationUtils $utils, FormFactoryInterface $factory): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // $form = $this->createForm(LoginType::class, ['email' => $utils->getLastUsername()]);
+        if ($this->getUser()) {
+            return $this->redirectToRoute('homepage');
+        }
 
-        $form = $factory->createNamed('', LoginType::class, ['_username' => $utils->getLastUsername()]);
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', [
-            'formView' => $form->createView(),
-            'error' => $utils->getLastAuthenticationError()
-        ]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     /**
      * @Route("/logout", name="security_logout")
      */
-    public function logout() {
-
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
